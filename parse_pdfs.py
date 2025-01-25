3a49dde4f6d2da9f1ba9aa0a7326f31b3d6a36f5
unknown <bhashyam.anirudh@gmail.com>
2025-01-25 15:58:34 +0530

    Basic parsing script. Doesn't work very well.

--- parse_pdfs.py
+++ parse_pdfs.py
@@ -1,38 +1,62 @@
+from alive_progress import alive_bar
+
 import camelot
+from camelot.core import Table, TableList
+
+import multiprocessing
 
 import pandas as pd
 
 from pathlib import Path
 
-from typing import Iterator
+import pickle
+
+from typing import (
+    Iterator,
+    Sequence,
+)
 
 
-def extract_tables_from_pdf(pdf_path: Path) -> Iterator[pd.DataFrame]:
+def extract_tables_from_pdf(pdf_path: Path) -> tuple[Path, TableList]:
     tables = camelot.read_pdf(
         str(pdf_path),
         pages = "all",
         flavor = "lattice",
+        flag_size = True,
+        table_areas = ["50,400,780,100"],
     )
-    print(f"Found {len(tables)} tables in {pdf_path.name}")
-    yield from (table.df for table in tables)
+    return pdf_path, tables
 
 
-def process_pdf_directory(pdf_dir: Path) -> Iterator[pd.DataFrame]:
-    results = {}
-    
-    for pdf_path in pdf_dir.glob("*.pdf"):
-        yield from extract_tables_from_pdf(pdf_path)
+def process_pdf_directory(pdf_dir: Path) -> list[Table]:
+    tables = []
+    paths = list(pdf_dir.glob("*.pdf"))
+    n_paths = len(paths)
+    with multiprocessing.Pool(4) as pool:
+        with alive_bar(title = "Parsing PDFs:", total = n_paths) as bar:
+            for p, df_list in pool.imap_unordered(
+                extract_tables_from_pdf,
+                pdf_dir.glob("*.pdf"),
+            ):
+                tables.extend(df_list)
+                bar()
+                bar.text(f"Done with '{p.stem}', found {len(df_list)} table(s).")
+    return tables
+
+
+def save_tables(tables: list[str]) -> None:
+    with open("tables.pkl", "wb") as f:
+        pickle.dump(tables, f)
 
 
 def main():
     pdf_dir = Path("pdf_data").resolve()
-    results = process_pdf_directory(pdf_dir)
-    pdf_tables = next(results)
-    
-    # print(f"\nProcessed {len(results)} PDF files")
-    # for pdf_name, tables in results.items():
-    #     print(f"{pdf_name}: {len(tables)} tables")
-
+    # tables = process_pdf_directory(pdf_dir)
+    _, tables = extract_tables_from_pdf(pdf_dir / "interim_report_february_2013.pdf")
+    camelot.plot(tables[10], kind = "grid").show()
+    breakpoint()
+    # save_tables(tables)
+    print(f"\nExtracted {len(tables)} total tables")
     return 0
 
 
